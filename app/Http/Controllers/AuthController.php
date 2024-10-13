aa63fe9cc107b93d860b59e4ae891512be76c5ac
zobirofkir <zobirofkir19@gmail.com>
2024-10-14 00:37:16 +0100

    create comment from auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,37 +16,78 @@ use Illuminate\Support\Facades\Password;
 
 class AuthController extends Controller
 {
+    /**
+     * Register a new user
+     *
+     * @param RegisterRequest $request
+     * @return void
+     */
     public function register(RegisterRequest $request)
     {
         return AuthFacade::register($request);
     }
 
+    /**
+     * Login a user
+     *
+     * @param LoginRequest $request
+     * @return void
+     */
     public function login(LoginRequest $request)
     {
         return AuthFacade::login($request);
     }
 
+    /**
+     * Get current user
+     *
+     * @return void
+     */
     public function current()
     {
         return AuthFacade::current();
     }
 
+    /**
+     * Forgot password
+     *
+     * @param ForgotPasswordRequest $request
+     * @return void
+     */
     public function forgotPassword(ForgotPasswordRequest $request)
     {
         return AuthFacade::forgotPassword($request);
     }
 
+    /**
+     * Show reset form
+     *
+     * @param ForgotPasswordRequest $request
+     * @param [type] $token
+     * @return void
+     */
     public function showResetForm(ForgotPasswordRequest $request, $token)
     {
         $token = AuthFacade::showResetForm($request , $token);
         return view('auth.passwords.reset', $token);
     }
 
+    /**
+     * Reset password
+     *
+     * @param ResetPasswordRequest $request
+     * @return void
+     */
     public function resetPassword(ResetPasswordRequest $request)
     {
         return AuthFacade::resetPassword($request);
     }
 
+    /**
+     * Logout a user
+     *
+     * @return void
+     */
     public function logout()
     {
         AuthFacade::logout();
